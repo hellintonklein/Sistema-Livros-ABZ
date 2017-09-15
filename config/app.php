448b6d601f83c1140a231edd6a1f701d5f5ebd2a
matfranco <matfranco2011@gmail.com>
2017-09-15 02:27:30 -0300

    Ajuste navbar

--- config/app.php
+++ config/app.php
@@ -230,7 +230,7 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'root',
-            'password' => 'root',
+            'password' => 'franco',
             'database' => 'trabalho_processo_software',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
